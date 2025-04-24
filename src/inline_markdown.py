8208492d95017ddbb7b0741fd77458b9369376f0
AlexRFarnes <alexrfarnes@gmail.com>
2025-04-24 17:47:26 +0800

    complete split images and links

---
+++ src/inline_markdown.py
@@ -0,0 +1,129 @@
+import re
+from textnode import TextType, TextNode
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        if old_node.text.count(delimiter) % 2 != 0: # delimiters should appear in pairs
+            raise ValueError("Unmatched delimiter found, invalid Markdwon syntax.")
+        
+        split_nodes = []
+        split_text = old_node.text.split(delimiter)
+        for i, text in enumerate(split_text):
+            if text == "":  # Skip empty segments
+                continue
+
+            if i % 2 == 0: # Even index: regular text
+                type = TextType.TEXT
+            else:  # Odd index: matched text inside delimiters
+                type = text_type
+            split_nodes.append(TextNode(text, type))
+
+        new_nodes.extend(split_nodes)
+
+    return new_nodes   
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        # find all matches in the text
+        matches = extract_markdown_images(old_node.text)
+
+        if not matches:
+            new_nodes.append(old_node)
+            continue
+
+        current_text = old_node.text
+
+        for alt_text, url in matches:
+            image = f"![{alt_text}]({url})"
+            sections = current_text.split(image, 1)
+
+            # add the text if not empty
+            if sections[0]:
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+
+            # add the image
+            new_nodes.append(TextNode(alt_text, TextType.IMAGE, url))
+
+            # safety check for when the image markdown is at the end, there is no sections[1] then.
+            if len(sections) > 1:
+                current_text = sections[1]
+            else: 
+                current_text = ""
+
+        if current_text:
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
+            
+            
+    return new_nodes
+        
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        matches = extract_markdown_links(old_node.text)
+
+        if not matches:
+            new_nodes.append(old_node)
+            continue
+
+        current_text = old_node.text
+
+        for text, url in matches:
+            link = f"[{text}]({url})"
+            sections = current_text.split(link, 1)
+
+            if sections[0]:
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+
+            new_nodes.append(TextNode(text, TextType.LINK, url))
+
+            if len(sections) > 1:
+                current_text = sections[1]
+            else:
+                current_text = ""
+        
+        if current_text:
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
+
+        return new_nodes
+
+
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+node = TextNode(
+    "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+    TextType.TEXT,
+)
+new_nodes = split_nodes_link([node])
+print(new_nodes)
+
+# [
+#     TextNode("This is text with a link ", TextType.TEXT),
+#     TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+#     TextNode(" and ", TextType.TEXT),
+#     TextNode(
+#         "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+#     ),
+# ]
--- src/test/test_inline_markdown.py
+++ src/test/test_inline_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from splitnode import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
 from textnode import TextNode, TextType
 
 class TestSplitNodeDelimiter(unittest.TestCase):
@@ -69,6 +69,60 @@ class TestSplitNodeDelimiter(unittest.TestCase):
                 TextNode(" text.", TextType.TEXT),
             ]
         )
+
+    def test_extract_markdown_images_regex(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        matches = extract_markdown_images(text)
+        self.assertEqual(matches, [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
+
+    def test_extract_markdown_images_regex(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        matches = extract_markdown_links(text)
+        self.assertEqual(matches, [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")])
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a [link](https://www.google.com)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://www.google.com"),
+            ],
+            new_nodes,
+        )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_link(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://www.google.com)"
+        )
+        self.assertListEqual([("link", "https://www.google.com")], matches)
 
 if __name__=="__main__":
     unittest.main()
