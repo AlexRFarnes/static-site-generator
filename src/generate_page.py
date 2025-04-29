cece7eb47e7dc8af2d4e2f71842aa454f4ae16c7
AlexRFarnes <alexrfarnes@gmail.com>
2025-04-30 05:59:00 +0800

    create the build script

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,7 +3,7 @@ from os import path, listdir, makedirs
 from pathlib import Path
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     dir_path = Path(dir_path_content)
     dest_path = Path(dest_dir_path)
 
@@ -12,14 +12,14 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         if current_path.is_file():
             name = current_path.stem
             generate_page(current_path, template_path,
-                          f"{dest_path}/{name}.html")
+                          f"{dest_path}/{name}.html", basepath)
         else:
             new_dest_path = dest_path.joinpath(path)
             generate_pages_recursive(
-                current_path, template_path, new_dest_path)
+                current_path, template_path, new_dest_path, basepath)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
 
     print(
         f"Generating page from {from_path} to {dest_path} using {template_path}\n")
@@ -37,6 +37,8 @@ def generate_page(from_path, template_path, dest_path):
 
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace("href=\"/", f"href=\"{basepath}")
+    template = template.replace("src=\"/", f"src=\"{basepath}")
 
     dest_dir_path = path.dirname(dest_path)
     if dest_dir_path != "":
--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
+import sys
 from pathlib import Path
 from generate_page import generate_pages_recursive
 from generate_static import generate_static_files
 
 
 def main():
+    try:
+        basepath = sys.argv[1]
+    except IndexError:
+        basepath = "/"
+
     from_path = Path.cwd().joinpath("static")
-    dest_path = Path.cwd().joinpath("public")
+    dest_path = Path.cwd().joinpath("docs")
     template_path = Path.cwd().joinpath("template.html")
     dir_path_content = Path.cwd().joinpath("content")
 
@@ -13,7 +19,8 @@ def main():
     generate_static_files(from_path, dest_path)
     # generate_page(md_path, template_path, index_path)
     print("Generating the pages...")
-    generate_pages_recursive(dir_path_content, template_path, dest_path)
+    generate_pages_recursive(
+        dir_path_content, template_path, dest_path, basepath)
 
 
 if __name__ == "__main__":
